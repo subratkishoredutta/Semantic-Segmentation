a634e6ba703c96af6b3cae3d847c75cbf4be7da2
SUBRAT KISHORE DUTTA <subratkishoredutta1234@gmail.com>
2021-01-06 23:30:48 +0530

    Add files via upload

---
+++ mobilenetV1.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  6 22:02:20 2021
+
+@author: Asus
+"""
+
+import os
+import numpy as np
+import cv2
+from glob import glob
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import keras
+from tqdm import tqdm
+from tensorflow.keras.layers import Conv2D, Activation, BatchNormalization
+from tensorflow.keras.layers import UpSampling2D, Input, Concatenate
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications import MobileNet,MobileNetV2
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+from tensorflow.keras.metrics import Recall, Precision
+from tensorflow.keras import backend as K
+
+IMAGE_SIZE= 224
+IMG_HEIGHT = 224
+IMG_CHANNELS = 3
+
+path = 'path to the original images'
+ 
+maskpath ='path to the mask of the images'
+ 
+X_train = np.zeros((len(os.listdir(path)), IMAGE_SIZE, IMAGE_SIZE,IMG_CHANNELS),dtype=np.uint8)
+Y_train = np.zeros((len(os.listdir(maskpath)), IMAGE_SIZE, IMAGE_SIZE,1))
+ 
+print('creating input data')
+n=0
+for name in tqdm(os.listdir(path)):
+    newpath = os.path.join(path,name)
+    img=cv2.imread(newpath)
+    img=cv2.resize(img,(IMAGE_SIZE,IMAGE_SIZE))
+    X_train[n]=img/255
+    n+=1
+ 
+m=0
+for name in tqdm(os.listdir(maskpath)):
+    newmaskpath = os.path.join(maskpath,name)
+    mpg=cv2.imread(newmaskpath,0)
+    mpg=cv2.resize(mpg,(IMAGE_SIZE,IMAGE_SIZE))
+    mpg=np.expand_dims(mpg,axis=-1)
+    Y_train[m]=mpg/255
+    m+=1
+    
+def modelmobileNetV1():
+  inputs = Input(shape=(224, 224, 3), name="input_image")
+  encoder = MobileNet(input_tensor=inputs, weights="imagenet", include_top=False, alpha=0.5)
+  skip_connection_names=["input_image","conv_pw_1_relu","conv_pw_3_relu", "conv_pw_5_relu"]
+  encoder_output=encoder.get_layer("conv_pw_11_relu").output
+  f = [32, 64, 128, 256]
+  x = encoder_output
+  for i in range(1, len(skip_connection_names)+1, 1):
+        x_skip = encoder.get_layer(skip_connection_names[-i]).output
+        x = UpSampling2D((2, 2))(x)
+        x = Concatenate()([x, x_skip])
+        
+        x = Conv2D(f[-i], (3, 3), padding="same")(x)
+        x = BatchNormalization()(x)
+        x = Activation("relu")(x)
+        
+        x = Conv2D(f[-i], (3, 3), padding="same")(x)
+        x = BatchNormalization()(x)
+        x = Activation("relu")(x)
+        
+  x = Conv2D(1, (1, 1), padding="same")(x)
+  x = Activation("sigmoid")(x)
+    
+  model = Model(inputs, x)
+  return model
+
+model=model1()
+opt=tf.keras.optimizers.Adam(0.0001)
+model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+model.summary()
+
+result = model.fit(X_train,Y_train,validation_split = 0.1,batch_size=16,epochs=2000)
+
+model.save('MObilenetV1.h5')
+
+
+##prediction
+
+img=model.predict(X_train[10:11],verbose=1)
+pred = (img>0.5).astype(np.uint8)
+plt.imshow(np.squeeze(pred))
+
+
+
+
